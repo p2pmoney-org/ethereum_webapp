111d99fe7d112320fa273cb6331e7fc5035712a2
fbaud <fbaud@p2pventure.org>
2020-01-24 11:43:33 +0100

    version 0.14.2

--- server/server.js
+++ server/server.js
@@ -23,8 +23,8 @@ if (process.env.ETHEREUM_WEBAPP_EXEC_DIR) {
 //instantiating global object
 var global = Global.getGlobalInstance();
 
-global.current_version = "0.14.0.2020.01.01";
-global.version_support = ["0.13", "0.12"];
+global.current_version = "0.14.2.2020.01.24";
+global.version_support = ["0.14", "0.13", "0.12"];
 
 
 //force logging
